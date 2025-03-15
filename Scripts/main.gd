2d45c1f6414955c97e1aabf8c77095c81205695a
waamer <108493771+Waamer@users.noreply.github.com>
2025-03-15 03:51:56 -0400

    Pipes almost working

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,4 +1,3 @@
-# Main scene script (attached to root node)
 extends Node3D
 
 @onready var timer: Timer = $Timer
@@ -7,16 +6,29 @@ extends Node3D
 var pipes_remaining := 0
 
 func _ready():
-	timer.timeout.connect(_on_timer_timeout)
+	# Initialize pipes
 	pipes_remaining = get_tree().get_nodes_in_group("pipes").size()
+	timer.start()
+	
+	# Connect all pipes
+	for pipe in get_tree().get_nodes_in_group("pipes"):
+		pipe.removed.connect(_on_pipe_removed)
 
 func _on_pipe_removed():
 	pipes_remaining -= 1
 	if pipes_remaining <= 0:
-		print("win")
+		show_victory()
 
 func _on_timer_timeout():
-	print("lost")
+	show_game_over()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	time_label.text = "Time: %.1f" % timer.time_left
+
+func show_victory():
+	print("All pipes removed! You win!")
+	# Add your victory logic here
+
+func show_game_over():
+	print("Time's up! Game Over!")
+	# Add your loss logic here
--- Scripts/pipe.gd
+++ Scripts/pipe.gd
@@ -1,30 +1,32 @@
 extends MeshInstance3D
 
-@export var move_speed: float = 2.0
+@export var move_distance: float = 1.0
+@export var move_speed: float = 0.5
 @export var shake_intensity: float = 0.1
+@export var is_horizontal: bool = true  # True = X-axis, False = Z-axis
 
 @onready var static_body: StaticBody3D = $StaticBody3D
-@onready var raycasts: Array[RayCast3D] = [$FrontRay, $BackRay]
+@onready var front_ray: RayCast3D = $FrontRay
+@onready var back_ray: RayCast3D = $BackRay
 
 var mouse_inside := false
 var move_direction := Vector3.ZERO
-var target_position := Vector3.ZERO
 var is_moving := false
-var can_remove := true
+signal removed
 
 func _ready():
-	await get_tree().process_frame
+	static_body.input_ray_pickable = true
 	static_body.mouse_entered.connect(_on_mouse_entered)
 	static_body.mouse_exited.connect(_on_mouse_exited)
 	
-	for ray in raycasts:
-		if ray:
-			ray.target_position = Vector3.ZERO
-			ray.force_raycast_update()
+	front_ray.add_exception(static_body)
+	back_ray.add_exception(static_body)
 	
-	if raycasts.size() > 0 && raycasts[0]:
-		var local_dir = raycasts[0].global_transform.basis.z
-		move_direction = local_dir.normalized()
+	# Set direction based on is_horizontal and ray target_position
+	if is_horizontal:
+		move_direction = Vector3.RIGHT if front_ray.target_position.x > 0 else Vector3.LEFT
+	else:
+		move_direction = Vector3.FORWARD if front_ray.target_position.z > 0 else Vector3.BACK
 
 func _on_mouse_entered():
 	mouse_inside = true
@@ -33,40 +35,57 @@ func _on_mouse_exited():
 	mouse_inside = false
 
 func try_remove():
-	if is_moving || !can_remove || !raycasts.all(func(r): return r != null):
+	if is_moving or not mouse_inside:
 		return
 	
-	for ray in raycasts:
-		ray.force_raycast_update()
-		if !ray.is_colliding():
-			var space_state = get_world_3d().direct_space_state
-			var query = PhysicsRayQueryParameters3D.create(
-				ray.global_position,
-				ray.global_position + ray.target_position
-			)
-			var result = space_state.intersect_ray(query)
-			if result:
-				target_position = result.position - move_direction * 0.1
-				start_movement()
-				return
-	shake_pipe()
+	# Force ray updates to detect moved pipes
+	front_ray.force_raycast_update()
+	back_ray.force_raycast_update()
+	
+	var can_move_front = not front_ray.is_colliding()
+	var can_move_back = not back_ray.is_colliding()
+	
+	print("Front Ray Colliding: ", front_ray.is_colliding())
+	print("Back Ray Colliding: ", back_ray.is_colliding())
+	
+	# Debug colliders
+	if front_ray.is_colliding():
+		print("Front Collider: ", front_ray.get_collider().name)
+	if back_ray.is_colliding():
+		print("Back Collider: ", back_ray.get_collider().name)
+	
+	if can_move_front:
+		move_pipe(move_direction)
+	elif can_move_back:
+		move_pipe(-move_direction)
+	else:
+		shake_pipe()
 
-func start_movement():
+func move_pipe(direction: Vector3):
+	if is_moving: 
+		return
+	
+	print("Moving Direction: ", direction)  # Debug
 	is_moving = true
-	var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	var target_position = global_position + (direction * move_distance)
+	print("Target Position: ", target_position)  # Debug
+	
+	var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "global_position", target_position, move_speed)
-	tween.tween_callback(func(): 
+	tween.tween_callback(func():
 		is_moving = false
-		move_direction *= -1
+		removed.emit()
 	)
 
 func shake_pipe():
+	var axis = "x" if is_horizontal else "z"
 	var orig_pos = global_position
-	var tween = create_tween().set_parallel(true)
-	tween.tween_property(self, "position:x", orig_pos.x + shake_intensity, 0.1)
-	tween.chain().tween_property(self, "position:x", orig_pos.x - shake_intensity, 0.1)
-	tween.chain().tween_property(self, "position:x", orig_pos.x, 0.1)
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "global_position:%s" % axis, orig_pos[axis] + shake_intensity, 0.1)
+	tween.chain().tween_property(self, "global_position:%s" % axis, orig_pos[axis] - shake_intensity, 0.1)
+	tween.chain().tween_property(self, "global_position:%s" % axis, orig_pos[axis], 0.1)
 
 func _unhandled_input(event):
-	if event.is_action_pressed("click") && mouse_inside:
+	if event.is_action_pressed("click") and mouse_inside:
 		try_remove()
